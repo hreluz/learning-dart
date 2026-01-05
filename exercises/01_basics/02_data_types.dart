cc16c8e950c1f02e3a7c4a88b96939019b8ef70f
Hector <hmreluz@gmail.com>
2026-01-05 10:32:50 -0500

    01:basics - number data type

---
+++ exercises/01_basics/02_data_types.dart
@@ -0,0 +1,18 @@
+main() {
+  // === Numbers
+  int a = 10;
+  double b = 5.5;
+
+  int? c;
+
+  int _a = 30;
+  double $b = 40;
+
+  double result = _a + $b;
+
+  print(_a + $b);
+
+  print(result);
+
+  print(c);
+}
