d9aa174a0e0ec02fbcd1165a7493b05039b6421e
Hector Reluz <hmreluz@gmail.com>
2026-01-06 11:14:33 -0500

    01:basics - var, final and const

---
+++ exercises/01_basics/03_finakconst.dart
@@ -0,0 +1,22 @@
+main() {
+  var a = 10;
+  final double b = 10;
+  const double c = 10;
+
+  // a = 20;
+  // b = 20;
+  // c = 20;
+
+  // final peopleFinal = ['Bruce', 'Clark', 'John'];
+  // const peopleConst = ['Bruce', 'Clark', 'John'];
+
+  final List<String> peopleFinal = ['Bruce', 'Clark', 'John'];
+  const List<String> peopleConst = ['Bruce', 'Clark', 'John'];
+  List<String> peopleConst2  = const['Bruce', 'Clark', 'John'];
+
+  peopleFinal.add('Diana');
+  peopleConst.add('Diana');
+
+  print(peopleFinal);
+  print(peopleConst);
+}
