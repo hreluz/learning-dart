78e518a2b4d2e6649fc5f8da7097ea9a4718ffbb
Hector Reluz <hmreluz@gmail.com>
2026-01-06 11:25:11 -0500

    01:basics - comments

---
+++ exercises/01_basics/04_comments.dart
@@ -0,0 +1,25 @@
+main() {
+  // this is a temporal variable
+  int a = 10;
+
+  /**
+   * 
+   * [createdOn]: today
+   */
+  final people = [
+    // comment 1
+    'Bruce',
+
+    // comment 2
+    'Clark',
+
+    // comment 3
+    'Barry'
+  ];
+
+
+  /// name: name of the person
+  /// gets a [name] and joins with [message]
+  greetings( int a, int b) {}
+
+}
