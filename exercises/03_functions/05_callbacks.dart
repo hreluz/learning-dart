8abbf1cbed4d4b283fddfbe9a0b86a013f5292a4
Hector <hmreluz@gmail.com>
2026-01-07 12:55:54 -0500

    03:functions - callback

---
+++ exercises/03_functions/05_callbacks.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  getUser('100', (Map person){ 
+    print(person);
+  });
+}
+
+void getUser(String id, Function callback) {
+  Map user =  {
+    'id': id,
+    'name' : 'Bruce',
+
+  };
+
+  callback(user);
+}
