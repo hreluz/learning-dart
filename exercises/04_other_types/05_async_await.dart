7994beba6b988874cf075732ffcfd49b130f81af
Hector <hmreluz@gmail.com>
2026-01-07 14:21:48 -0500

    04:other types - async-await

---
+++ exercises/04_other_types/05_async_await.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(List<String> args) async {
+
+  String path = Directory.current.path + '/exercises/04_other_types/assets/people.txt';
+
+  await readFile(path).then(print);
+
+
+  print('end of main');
+}
+
+readFile(String path) async {
+  File file = new File(path);
+
+  return file.readAsString();
+}
+
+// Future<String> readFile(String path) {
+//   File file = new File(path);
+
+//   return file.readAsString();
+// }
