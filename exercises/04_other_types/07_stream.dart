90411e8bf93e40af718a7e376014d1d716901a43
Hector <hmreluz@gmail.com>
2026-01-07 14:50:58 -0500

    04:other types - streams - onError, onDone and cancelOnError

--- exercises/04_other_types/07_stream.dart
+++ exercises/04_other_types/07_stream.dart
@@ -3,12 +3,21 @@ import 'dart:async';
 void main(List<String> args) {
   final streamController = StreamController();
 
-  streamController.stream.listen((data) {
-    print('Launching! $data');
-  });
+  streamController.stream.listen(
+    (data) =>  print('Launching! $data'),
+    onError: (err) => print('Error! $err'),
+    cancelOnError: true,
+    onDone: () => print('Mission accomplished')
+  );
 
 
   streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  // streamController.sink.addError('Houston, we have an issue');
+  streamController.sink.add('Apollo 14');
+
+  streamController.sink.close();
+  // streamController.sink.add('Apollo 15');
 
   print('end of main');
 }
