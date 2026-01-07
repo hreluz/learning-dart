5e035e25d62b2db412c0400d54f24c5436f8fe7d
Hector <hmreluz@gmail.com>
2026-01-07 12:26:50 -0500

    03:functions - args by name

--- exercises/03_functions/02_arguemnts.dart
+++ exercises/03_functions/02_arguemnts.dart
@@ -2,6 +2,11 @@ void greetings(String message, [ String name = '<insert name>']) {
   print('$message, $name');
 }
 
+void greetings2(String? message, {required String name, int times = 10} ) {
+  print('Greetings2: $message $name - $times');
+}
+
 main() {
   greetings('Hello', 'Bruce');
+  greetings2('Hey', name: "Batman", times: 200);
 }
