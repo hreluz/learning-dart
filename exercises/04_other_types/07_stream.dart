ea685a242e311ac75d95af108a5a62b593056275
Hector <hmreluz@gmail.com>
2026-01-07 14:53:52 -0500

    04:other types - streams - type and broadcast

--- exercises/04_other_types/07_stream.dart
+++ exercises/04_other_types/07_stream.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 void main(List<String> args) {
-  final streamController = StreamController();
+  // StreamController<String> streamController = StreamController();
+  final streamController = StreamController<String>.broadcast();
 
   streamController.stream.listen(
     (data) =>  print('Launching! $data'),
@@ -10,11 +11,19 @@ void main(List<String> args) {
     onDone: () => print('Mission accomplished')
   );
 
+  streamController.stream.listen(
+    (data) =>  print('Launching 2! $data'),
+    onError: (err) => print('Error 2! $err'),
+    cancelOnError: true,
+    onDone: () => print('Mission accomplished 2')
+  );
+
 
   streamController.sink.add('Apollo 11');
   streamController.sink.add('Apollo 12');
   // streamController.sink.addError('Houston, we have an issue');
   streamController.sink.add('Apollo 14');
+  // streamController.sink.add(23);
 
   streamController.sink.close();
   // streamController.sink.add('Apollo 15');
