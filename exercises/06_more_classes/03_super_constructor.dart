44ea65da43107932e86a961cac6b1ebb20d2bc6c
Hector <hmreluz@gmail.com>
2026-01-09 13:49:22 -0500

    06: more classes: super constructor

---
+++ exercises/06_more_classes/03_super_constructor.dart
@@ -0,0 +1,24 @@
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+
+  void printName() => print('Name: $name, Age: $age');
+}
+
+class Client extends Person {
+  String? address;
+  List orders = [];
+
+  Client(int currentAge, String currentName):
+    super(currentName, currentAge);
+}
+
+void main(List<String> args) {
+
+  final me = new Client(33, 'Bruce');
+
+  me.printName();
+  
+}
