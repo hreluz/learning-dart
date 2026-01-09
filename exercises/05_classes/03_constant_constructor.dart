423353b951b2ecf12396fb7e112040925df7fdd5
Hector <hmreluz@gmail.com>
2026-01-09 11:48:55 -0500

    05: intro to classes: constant constructor

---
+++ exercises/05_classes/03_constant_constructor.dart
@@ -0,0 +1,26 @@
+class Location {
+  final double lat;
+  final double lng;
+
+  const Location(this.lat, this.lng);
+}
+
+void main(List<String> args) {
+
+  final sanFrancisco1 = new Location(18.2323, 39.9001);
+  final sanFrancisco2 = new Location(18.2323, 39.9000);
+  final sanFrancisco3 = new Location(18.2323, 39.9000);
+  
+  // print(sanFrancisco1 == sanFrancisco2);
+  // print(sanFrancisco2 == sanFrancisco3);
+
+
+  const sanFrancisco4 = const Location(18.2323, 39.9001);
+  const sanFrancisco5 = const Location(18.2323, 39.9000);
+  const sanFrancisco6 = const Location(18.2323, 39.9000);
+
+
+  print(sanFrancisco4 == sanFrancisco5);
+  print(sanFrancisco5 == sanFrancisco6);
+
+}
